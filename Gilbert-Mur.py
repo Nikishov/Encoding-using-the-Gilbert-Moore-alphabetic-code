b0b239cb9316cef7734b52cf4e13ef3afc93b8de
Daniil <52314094+Nikishov@users.noreply.github.com>
2020-04-15 13:40:40 +0300

    Add files via upload

---
+++ Gilbert-Mur.py
@@ -0,0 +1,45 @@
+import math as m
+import collections
+
+def Translate(arg, l):
+            num = ""
+            for i in range(0,l):
+                arg = arg * 2.0
+                num += str(m.trunc(arg))
+                if arg >= 1.0:
+                    arg -= 1.0
+            return num
+			
+def gilbertMur():
+	text = input()
+	coded_text=''
+	id_2 = {}
+	decode = {}
+	sum=0.0
+	for i in text:
+		if i not in id_2:
+			id_2[i.lower()] = [1,0,0,'']
+		else:
+			id_2[i][0] += 1
+	id_2 = collections.OrderedDict(sorted(id_2.items(),key = lambda i:i[0]))
+	for i in id_2:
+		id_2[i][0] /= len(text)
+		id_2[i][1] = m.ceil(m.log2(1 / id_2[i][0])) + 1
+		id_2[i][2] = id_2[i][0] / 2 + sum
+		id_2[i][3] = Translate(id_2[i][2], id_2[i][1])
+		sum+=id_2[i][0]
+	for i in text.lower():
+		coded_text += id_2[i][3]
+	print(coded_text)
+	for j, i in id_2.items():
+		decode[i[3]] = j
+	count = ''
+	for i in coded_text:
+		try:
+			count += i
+			print(decode[count], end = ' ')
+			count = ''
+		except:
+			pass
+
+gilbertMur()
